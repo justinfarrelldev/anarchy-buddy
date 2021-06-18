e62636448958418d39f28c8f2b33d222d0c7bfee
justinfarrelldev <justinfarrellgamedev@gmail.com>
2021-06-18 16:08:42 -0400

    Wrote up unit tests for command list

---
+++ src/test/user.test.ts
@@ -0,0 +1,51 @@
+import { UserList, UsingCommandList } from "../user";
+
+describe("UserList", () => {
+  it("can be added to", () => {
+    const userList = new UserList();
+    userList.AddToUsingCommandList({
+      username: "Test",
+      discriminator: "1234",
+      commandPredicate: "help",
+    } as UsingCommandList);
+
+    expect(userList.usingCommandList).toHaveLength(1);
+    expect(userList.usingCommandList[0]).toMatchObject({
+      username: "Test",
+      discriminator: "1234",
+      commandPredicate: "help",
+    } as UsingCommandList);
+  });
+});
+
+describe("UserList", () => {
+  it("can be removed from", () => {
+    const userList = new UserList();
+    userList.AddToUsingCommandList({
+      username: "New Test",
+      discriminator: "1234",
+      commandPredicate: "help",
+    } as UsingCommandList);
+
+    userList.AddToUsingCommandList({
+      username: "New Test 2",
+      discriminator: "4321",
+      commandPredicate: "create",
+    } as UsingCommandList);
+
+    userList.RemoveFromUsingCommandList({
+      username: "New Test",
+      discriminator: "1234",
+      commandPredicate: "help",
+    } as UsingCommandList);
+
+    console.log("USER LIST: ", userList.usingCommandList);
+
+    expect(userList.usingCommandList).toHaveLength(1);
+    expect(userList.usingCommandList[0]).toMatchObject({
+      username: "New Test 2",
+      discriminator: "4321",
+      commandPredicate: "create",
+    } as UsingCommandList);
+  });
+});
--- src/user.ts
+++ src/user.ts
@@ -1,13 +1,13 @@
 import { COMMAND_LIST } from "./commands/index";
 
-interface UsingCommandList {
+export interface UsingCommandList {
   username: string;
   discriminator: string;
-  commandPredicate: typeof COMMAND_LIST;
+  commandPredicate: string;
 }
 
 export class UserList {
-  usingCommandList: UsingCommandList[];
+  usingCommandList: UsingCommandList[] = [];
   /**
    * Creates an input array which is only accessible through getters and setters to set the users that are
    * currently using a command
@@ -17,8 +17,8 @@ export class UserList {
     this.usingCommandList.push(newEntry);
 
   public RemoveFromUsingCommandList = (entryToRemove: UsingCommandList) =>
-    this.usingCommandList.splice(
+    (this.usingCommandList = this.usingCommandList.splice(
       this.usingCommandList.indexOf(entryToRemove),
       1
-    );
+    ));
 }
