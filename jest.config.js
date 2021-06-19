8293e83502b3bc2714426461330a376cbf8829c8
justinfarrelldev <justinfarrellgamedev@gmail.com>
2021-06-19 18:17:25 -0400

    jest-junit added for test results in XML format

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  modulePathIgnorePatterns: ["build"] 
+  modulePathIgnorePatterns: ["build"],
+  reporters: [
+    "default",
+    "jest-junit" 
+  ]
 };
